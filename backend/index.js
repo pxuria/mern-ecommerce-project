d4e565f35a2925ad837e6e7b1c42db142d8f46bd
pxuria <pouriamesram@gmail.com>
2024-06-15 15:15:25 -0700

    init v2

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,10 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 connectDB();
-
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extends: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
