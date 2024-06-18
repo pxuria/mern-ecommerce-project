1b7178528d82ca9b271c7148229a190850171b42
پوریا مصر <p.mesr@ezgroup.me>
2024-06-18 16:49:13 +0330

    fix create user api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,40 @@
-import User from "../models/userModel";
-import asyncHandler from "../middlewares/asyncHandler";
+import User from "../models/userModel.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
 
 const createUser = asyncHandler(async (req, res) => {
-  res.send("hello");
+  const { username, email, password } = req.body;
+  console.log(username, email, password);
+
+  if (!username || !email || !password)
+    throw new Error("please fill the inputs");
+
+  const userExists = await User.findOne({ email });
+  if (userExists)
+    res.status(400).json({ status: "fail", message: "user alreaduy existed" });
+
+  const newUser = new User({ username, email, password });
+
+  try {
+    await newUser.save();
+    res.status(201).json({
+      status: "success",
+      message: "new user created",
+      data: {
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        password: newUser.password,
+        isAdmin: newUser.isAdmin,
+      },
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("please provide a new user");
+  }
 });
-const getUsers = async () => {};
-const getUser = async () => {};
-const deleteUser = async () => {};
-const updateUser = async () => {};
+// const getUsers = async () => {};
+// const getUser = async () => {};
+// const deleteUser = async () => {};
+// const updateUser = async () => {};
 
 export { createUser };
--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,12 @@ const port = process.env.PORT || 3000;
 
 connectDB();
 const app = express();
-app.use("/api/users", userRoutes);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("hello world");
