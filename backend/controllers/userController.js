07a44606f18da5e69dcbef30575888252562e19d
پوریا مصر <p.mesr@ezgroup.me>
2024-06-18 17:00:18 +0330

    fix create user api v2

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
+import bcrypt from "bcryptjs";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
@@ -12,7 +13,10 @@ const createUser = asyncHandler(async (req, res) => {
   if (userExists)
     res.status(400).json({ status: "fail", message: "user alreaduy existed" });
 
-  const newUser = new User({ username, email, password });
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
     await newUser.save();
@@ -23,7 +27,7 @@ const createUser = asyncHandler(async (req, res) => {
         _id: newUser._id,
         username: newUser.username,
         email: newUser.email,
-        password: newUser.password,
+        // password: newUser.password,
         isAdmin: newUser.isAdmin,
       },
     });
