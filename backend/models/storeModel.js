dce7f2c9d468bd05dc449ad4123d2bdfd5781301
پوریا مصر <p.mesr@ezgroup.me>
2024-08-03 17:09:48 +0330

    create store interface apu

--- backend/models/storeModel.js
+++ backend/models/storeModel.js
@@ -1,5 +1,17 @@
 import mongoose from "mongoose";
 
-const store = mongoose.Schema({
+const storeSchema = mongoose.Schema({
   name: { type: String, required: true },
+  address: {
+    city: { type: String, required: true },
+    province: { type: String, required: true },
+    address: { type: String, required: true },
+  },
+  description: { type: String },
+  phoneNumber: { type: Number, required: true },
+  email: { type: String },
+  products: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
 });
+
+const store = mongoose.model("Store", storeSchema);
+export default store;
