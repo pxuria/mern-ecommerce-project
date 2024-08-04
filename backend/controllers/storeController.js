fa7a3e57f38ca304c66912ca5d0eede4090cf238
pxuria <pouriamesram@gmail.com>
2024-08-04 08:50:32 -0700

    create get all stores api

--- backend/controllers/storeController.js
+++ backend/controllers/storeController.js
@@ -1,3 +1,34 @@
-import store from "../models/storeModel";
+import asyncHandler from "../middlewares/asyncHandler.js";
+import Store from "../models/storeModel.js";
 
-export {};
+const getAllStores = asyncHandler(async (req, res) => {
+  try {
+    const stores = await Store.find({});
+    res.status(200).json({ status: "success", results: stores.length, data: stores });
+  } catch (error) {}
+  console.log(error);
+  res.status(400).json(error.message);
+});
+
+const createStore = asyncHandler(async (req, res) => {
+  try {
+    const { name, description, address, phoneNumber } = req.body;
+
+    // validations
+    switch (true) {
+      case !name:
+        return res.status(400).json({ error: "name is required" });
+      case !description:
+        return res.status(400).json({ error: "description is required" });
+      case !address:
+        return res.status(400).json({ error: "address is required" });
+      case !phoneNumber:
+        return res.status(400).json({ error: "phoneNumber is required" });
+    }
+    const store = new Store({ ...req.body, owner: req.user._id });
+    await store.save();
+    res.status(201).json({ status: "success ", data: { store } });
+  } catch (error) {}
+});
+
+export { getAllStores, createStore };
--- backend/models/storeModel.js
+++ backend/models/storeModel.js
@@ -4,6 +4,10 @@ const storeSchema = mongoose.Schema(
   {
     name: { type: String, trim: true, required: true },
     address: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
+    email: { type: String },
+    image: { type: String },
     products: [
       {
         type: mongoose.Schema.Types.ObjectId,
@@ -21,5 +25,5 @@ const storeSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-const store = mongoose.model("Store", storeSchema);
-export default store;
+const Store = mongoose.model("Store", storeSchema);
+export default Store;
