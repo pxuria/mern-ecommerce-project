aaec450ca75a2feadc290bc8717cdb3a78aafcbb
pxuria <pouriamesram@gmail.com>
2024-08-04 06:21:50 -0700

    create store model schema

--- backend/models/storeModel.js
+++ backend/models/storeModel.js
@@ -1,5 +1,25 @@
 import mongoose from "mongoose";
 
-const store = mongoose.Schema({
-  name: { type: String, required: true },
-});
+const storeSchema = mongoose.Schema(
+  {
+    name: { type: String, trim: true, required: true },
+    address: { type: String, required: true, trim: true },
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Products",
+      },
+    ],
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    categories: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const store = mongoose.model("Store", storeSchema);
+export default store;
