a92008bea1a7095d151d3d625d00b4ffa4852422
پوریا مصر <p.mesr@ezgroup.me>
2024-08-13 12:36:34 +0330

    fix get stores api

--- backend/routes/storeRotues.js
+++ backend/routes/storeRotues.js
@@ -1,9 +1,19 @@
 import express from "express";
 const router = express.Router();
-import { createStore, getAllStores, deleteStore, getStore, updateStore } from "../controllers/storeController.js";
+import {
+  createStore,
+  getAllStores,
+  deleteStore,
+  getStore,
+  updateStore,
+} from "../controllers/storeController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
-router.route("/").get(authenticate, authorizeAdmin, getAllStores).post(authenticate, createStore);
-router.route("/:storeId").get(authenticate, getStore).put(authenticate, updateStore).delete(authenticate, deleteStore);
+router.route("/").get(getAllStores).post(authenticate, createStore);
+router
+  .route("/:storeId")
+  .get(authenticate, getStore)
+  .put(authenticate, updateStore)
+  .delete(authenticate, deleteStore);
 
 export default router;
