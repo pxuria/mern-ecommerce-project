371560cc8a67ae0c600b23b39fa37efb0588ffc4
پوریا مصر <p.mesr@ezgroup.me>
2024-08-14 10:30:05 +0330

    clear code

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 //packages
-import path from "path";
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
@@ -22,9 +22,9 @@ const port = process.env.PORT || 3000;
 connectDB();
 
 const app = express();
+
 app.use(helmet());
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
