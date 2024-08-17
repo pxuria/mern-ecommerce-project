9b5a937d1ef266e371e760ce8c444775a2fcb927
پوریا مصر <p.mesr@ezgroup.me>
2024-08-17 14:06:30 +0330

    create update store api

--- backend/controllers/storeController.js
+++ backend/controllers/storeController.js
@@ -7,9 +7,14 @@ import User from "../models/userModel.js";
 const getAllStores = asyncHandler(async (req, res) => {
   try {
     const stores = await Store.find({});
-    if (!stores.length) return res.status(200).json({ status: "success", message: "there is no stores", data: [] });
+    if (!stores.length)
+      return res
+        .status(200)
+        .json({ status: "success", message: "there is no stores", data: [] });
 
-    res.status(200).json({ status: "success", results: stores.length, data: stores });
+    res
+      .status(200)
+      .json({ status: "success", results: stores.length, data: stores });
   } catch (error) {
     console.log(error);
     res.status(400).json(error.message);
@@ -22,7 +27,8 @@ const createStore = asyncHandler(async (req, res) => {
   const userId = req.headers.userid;
 
   const existingStore = await Store.findOne({ owner: userId });
-  if (existingStore) return res.status(400).json({ message: "User already owns a store" });
+  if (existingStore)
+    return res.status(400).json({ message: "User already owns a store" });
 
   // validations
   switch (true) {
@@ -91,23 +97,54 @@ const getStore = asyncHandler(async (req, res) => {
 
 const updateStore = asyncHandler(async (req, res) => {
   try {
+    const { storeId } = req.params;
     const { name, address, description, phoneNumber, image } = req.body;
+
+    const store = await Store.findById(storeId);
+    if (!store) return res.status(404).json({ error: "Store not found" });
+
+    // Update the store fields if they are provided in the request
+    if (name) store.name = name;
+    if (address) store.address = address;
+    if (description) store.description = description;
+    if (phoneNumber) store.phoneNumber = phoneNumber;
+    if (image) store.image = image;
+
+    // Save the updated store
+    const updatedStore = await store.save();
+    res.status(200).json({
+      status: "success",
+      message: "Store updated successfully",
+      data: updatedStore,
+    });
   } catch (error) {
-    console.log(error);
-    res.status(400).json(error.message);
+    console.error(error);
+    res.status(400).json({ error: error.message });
   }
 });
 
 const getProductsByStore = asyncHandler(async (req, res) => {
   const { storeId } = req.params;
-  const products = await Product.find({ store: storeId }).populate("store", "name address owner");
+  const products = await Product.find({ store: storeId }).populate(
+    "store",
+    "name address owner"
+  );
 
   if (products && products.length > 0)
-    res.status(200).json({ status: "success", results: products.length, data: products });
+    res
+      .status(200)
+      .json({ status: "success", results: products.length, data: products });
   else {
     res.status(404);
     throw new Error("No products found for this store");
   }
 });
 
-export { getAllStores, createStore, deleteStore, getStore, updateStore, getProductsByStore };
+export {
+  getAllStores,
+  createStore,
+  deleteStore,
+  getStore,
+  updateStore,
+  getProductsByStore,
+};
